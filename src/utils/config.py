479357052d96d84dfa36a2fcf995420347b225f9
Eclipse <Eclips77@users.noreply.github.com>
2025-09-01 18:02:27 +0300

    Add comprehensive weapon list to weapon_list.txt

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,8 +5,8 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
-CSV_FILE_PATH = os.path.join(DATA_DIR, "weapon_list.txt")
-WEAPONS_FILE_PATH = os.path.join(DATA_DIR, "another_file.csv")
+CSV_FILE_PATH = os.path.join(DATA_DIR, "tweets3.csv")
+WEAPONS_FILE_PATH = os.path.join(DATA_DIR, "weapon_list.txt")
 
 
 # CSV_FILE_PATH = os.getenv("CSV_PATH","C:/Users/brdwn/Desktop/my_projects/Python/Elastic/Week_12_ElasticSearch_Malicious_Text/data/tweets_injected 3.csv")
