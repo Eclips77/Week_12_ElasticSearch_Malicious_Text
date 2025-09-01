9c6db35a83bd9ea5cf8f2d05c801dfdf2ee18bee
Eclipse <Eclips77@users.noreply.github.com>
2025-09-01 11:13:28 +0300

    Refactor ESClient and DataLoader classes; update logging messages and improve data loading methods

--- src/services/loader.py
+++ src/services/loader.py
@@ -1,50 +1,34 @@
-import pandas as pd
 import logging
+import pandas as pd
+
 logger = logging.getLogger(__name__)
 
 class DataLoader:
     """"
-    DataLoader class for loading CSV and TXT files.
+    loader class for svc files or txt
     """
     @staticmethod
-    def load_data(file_path:str)-> pd.DataFrame:
-        """Load a CSV file.
-
-        Args:
-            file_path (str): Path to the CSV file.
-
-        Returns:
-            pandas.DataFrame: Loaded DataFrame.
-
-        Usage:
-            df = DataLoader.load_data("data.csv")
+    def load_from_csv(file_path: str)->pd.DataFrame:
+        """"
+        a method to load data from csv file
         """
         try:
-            df = pd.read_csv(file_path)
+            df = pd.DataFrame(file_path)
             logger.info("data loaded secssefuly")
             return df
         except Exception as e:
-            logger.error(f"Error loading data: {e}")
-            raise
+            logger.error(f"error loading csv file {e}")
+            raise e
 
     @staticmethod
-    def load_txt(file_path:str)->list:
-        """Load a TXT file.
-
-        Args:
-            file_path (str): Path to the TXT file.
-
-        Returns:
-            list: List of lines from the TXT file.
-
-        Usage:
-            lines = DataLoader.load_txt("data.txt")
+    def load_from_txt(file_path: str)->pd.DataFrame:
+        """"
+        a method to load data from txt file
         """
         try:
-            with open(file_path, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
-            logger.info("TXT data loaded secssefuly")
-            return lines
+            df = pd.read_csv(file_path, delimiter="\t", header=None)
+            logger.info("data loaded secssefuly")
+            return df
         except Exception as e:
-            logger.error(f"Error loading TXT data: {e}")
-            raise
+            logger.error(f"error loading txt file {e}")
+            raise e
